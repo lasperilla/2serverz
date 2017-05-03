2110366647c0d1382f4933d6ac9ac43d18d01cda
lasperilla <l.asperilla@gmail.com>
2017-05-02 20:03:01 -0400

    added server2. just needed one server.js file. will remove server2.js

--- server1.js
+++ server1.js
@@ -11,3 +11,16 @@ var server = http.createServer(handleRequest);
 server.listen(PORT, function(){
 	console.log("Port: http://localhost:%s", PORT)
 });
+
+const PORT2 = 7500;
+
+
+function handleRequest2(request, response) {
+	response.end("There's something on this pizza that I don't like. More toppings!")
+};
+
+var server2 = http.createServer(handleRequest2);
+
+server2.listen(PORT2, function(){
+	console.log("Port2: http://localhost:%s", PORT2)
+});
