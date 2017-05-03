5eeec55a0e1c24933b2f72d8a3ebeadf3e26473a
lasperilla <l.asperilla@gmail.com>
2017-05-02 20:20:37 -0400

    changed it to ouput random insult/compliment from array

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const http = require('http');
 const PORT = 7000;
+const PORT2 = 7500;
 
+let complimentArr = ["You have very nice pizza.", "Your apartment smells of rich mahogany.", "You're the man now dog!"]
 
 function handleRequest(request, response) {
-	response.end("You have very nice pizza.")
+    let randCompliment = complimentArr[Math.floor(Math.random() * complimentArr.length)];
+    response.end(randCompliment);
 };
 
 var server = http.createServer(handleRequest);
 
-server.listen(PORT, function(){
-	console.log("Port: http://localhost:%s", PORT)
+server.listen(PORT, function() {
+    console.log("Port: http://localhost:%s", PORT)
 });
 
-const PORT2 = 7500;
+//============================
 
+let insultsArr = ["There's something on this pizza that I don't like.", "More toppings!", "Hey! Where Did You Get Those Clothes. At The Toilet Store?"]
 
 function handleRequest2(request, response) {
-	response.end("There's something on this pizza that I don't like. More toppings!")
+    let randInsult = insultsArr[Math.floor(Math.random() * insultsArr.length)];
+    response.end(randInsult);
 };
 
 var server2 = http.createServer(handleRequest2);
 
-server2.listen(PORT2, function(){
-	console.log("Port2: http://localhost:%s", PORT2)
+server2.listen(PORT2, function() {
+    console.log("Port2: http://localhost:%s", PORT2)
 });
