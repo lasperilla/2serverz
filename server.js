4ecc20bb9dbfc39194873773758ec9b718c579e0
lasperilla <l.asperilla@gmail.com>
2017-05-02 20:29:31 -0400

    changed insult array to bangarang

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const PORT = 7000;
 const PORT2 = 7500;
 
-let complimentArr = ["You have very nice pizza.", "Your apartment smells of rich mahogany.", "You're the man now dog!"]
+let complimentArr = ["You remind me of pizza", "Your apartment smells of rich mahogany.", "You're the man now dog!", "10 10 10: You"]
 
 function handleRequest(request, response) {
     let randCompliment = complimentArr[Math.floor(Math.random() * complimentArr.length)];
@@ -17,7 +17,7 @@ server.listen(PORT, function() {
 
 //============================
 
-let insultsArr = ["There's something on this pizza that I don't like.", "More toppings!", "Hey! Where Did You Get Those Clothes. At The Toilet Store?"]
+let insultsArr = ["Boil-dripping beef, fart-sniffing bubble butt!", "You are fart factory, slug-slimed, sack-of-rat-guts-in-cat-vomit, cheesy, scab-picked, pimple-squeezing finger bandage. A week old maggot burger with everything on it and flies on the side!", "Substitute chemistry teacher.", "Mung tongue.", "Math Tutor", "Prison Barber", "Mother Lover", "Nearsighted gynecologist.", "In your face, camel cake!", "In your rear, cow derriere.", "Lying, crying, spying, prying ultra-pig.", "You lewd, crude, rude, bag of pre-chewed food dude."]
 
 function handleRequest2(request, response) {
     let randInsult = insultsArr[Math.floor(Math.random() * insultsArr.length)];
